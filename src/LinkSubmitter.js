f8d327946b37cecfcca1f0f7229532a3daa252bf
Jose Nery <josenerydev@gmail.com>
2023-09-19 16:41:33 -0300

    ajustes

--- src/LinkSubmitter.js
+++ src/LinkSubmitter.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaste } from '@fortawesome/free-solid-svg-icons';
 
 function LinkSubmitter() {
     const [link, setLink] = useState('');
@@ -6,6 +8,16 @@ function LinkSubmitter() {
 
     const containsQrCode = (link) => {
         return link.includes('qrcode');
+    }
+
+    const handlePaste = () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                setLink(text);
+            })
+            .catch(err => {
+                console.error('Erro ao colar o texto: ', err);
+            });
     }
 
     const handleSubmit = async () => {
@@ -47,7 +59,12 @@ function LinkSubmitter() {
                 value={link}
                 onChange={e => setLink(e.target.value)}
             />
-            <button className="link-button" onClick={handleSubmit}>Enviar</button>
+            <div className="button-group">
+                <button className="paste-button" onClick={handlePaste}>
+                    <FontAwesomeIcon icon={faPaste} /> Colar
+                </button>
+                <button className="link-button" onClick={handleSubmit}>Enviar</button>
+            </div>
             {message && <div className="message">{message}</div>}
         </div>
     );
